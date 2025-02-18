1c63730733317dea20b68b76fc7b0abb08593d78
O. HADALIN <dyblikat777@gmail.com>
2025-02-18 16:23:36 +0200

    s5: add pull all-contacts

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,13 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    return contacts;
+  } catch (error) {
+    console.log(`Error fetching contacts:`, error);
+    return [];
+  }
+};
 
 console.log(await getAllContacts());
