d79d4849bfdca86fcd83226be0db12c28cf58a34
O. HADALIN <dyblikat777@gmail.com>
2025-02-18 17:04:17 +0200

    s8: add removeLast

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,19 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
-removeLastContact();
+export const removeLastContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    if (contacts.length === 0) {
+      console.log(`No contacts to remove.`);
+      return;
+    }
+    contacts.pop();
+    await writeContacts(contacts);
+    console.log(`Last contact has been removed!`);
+  } catch (error) {
+    console.error('Error removing last contacts:', error);
+  }
+};
+
+removeLastContacts();
