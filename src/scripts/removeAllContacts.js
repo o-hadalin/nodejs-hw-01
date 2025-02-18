d00e85e71c14cbfcb522577eaa2032d138113a88
O. HADALIN <dyblikat777@gmail.com>
2025-02-18 16:49:51 +0200

    mod removeAll: empty db check

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,7 +1,13 @@
+import { readContacts } from '../utils/readContacts.js';
 import { writeContacts } from '../utils/writeContacts.js';
 
 export const removeAllContacts = async () => {
   try {
+    const contacts = await readContacts();
+    if (contacts.length === 0) {
+      console.log(`No contacts to remove.`);
+      return;
+    }
     await writeContacts([]);
     console.log(`All contacts have been removed!`);
   } catch (error) {
