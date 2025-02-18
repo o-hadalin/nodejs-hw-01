901f6bf250e9a562f525c80122e6d0c3c1c7d892
O. HADALIN <dyblikat777@gmail.com>
2025-02-18 15:05:37 +0200

    s2: read&write functions

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const data = await fs.promises.readFile(PATH_DB, 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Error reading the contacts:', error);
+    return [];
+  }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const data = JSON.stringify(updatedContacts, null, 2);
+    await fs.promises.writeFile(PATH_DB, data, 'utf-8');
+    console.log('Contacts have been successfully written to the file.');
+  } catch (error) {
+    console.error('Error writing the contacts:', error);
+    return [];
+  }
+};
