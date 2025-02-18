135fb957361890391a23c01d6ed33226d4b703fb
O. HADALIN <dyblikat777@gmail.com>
2025-02-18 16:43:49 +0200

    s7: add removeAll, dell log from writeContacts

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -5,7 +5,6 @@ export const writeContacts = async (updatedContacts) => {
   try {
     const data = JSON.stringify(updatedContacts, null, 2);
     await fs.promises.writeFile(PATH_DB, data, 'utf-8');
-    console.log('Contacts have been successfully written to the file.');
   } catch (error) {
     console.error('Error writing the contacts:', error);
     return [];
